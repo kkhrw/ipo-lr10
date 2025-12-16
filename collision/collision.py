13955cca111b4a1c9445cbe1fcf6160d4c657cc0
uljana.korolenko@gmail.com <uljana.korolenko@gmail.com>
2025-12-16 20:38:27 +0300

    Task 4 completed

--- collision/collision.py
+++ collision/collision.py
@@ -1,3 +1,5 @@
+class RectCorrectError(Exception):
+  pass
 def isCorrectRect(coords):
   bottom_left, top_right = coords
   x1, y1 = bottom_left
@@ -6,21 +8,19 @@ def isCorrectRect(coords):
   return x1 < x2 and y1 < y2
 
 
-print(isCorrectRect([(-3.4, 1), (9.2, 10)]))
-print(isCorrectRect([(-7, 9), (3, 6)]))  
 
-def isCollisionRect(rect1,rect2):
-    if not isCorrectRect(rect1):
-        raise RectCorrectError("1й прямоугольник некоректный")
-    if not isCorrectRect(rect2):
-        raise RectCorrectError("2й прямоугольник некоректный")
-    
-    x1_min, y1_min = rect1[0]
-    x1_max, y1_max = rect1[1]
-    x2_min, y2_min = rect2[0]
-    x2_max, y2_max = rect2[1]
-
-    if x1_max <= x2_min or x2_max <= x1_min or y1_max <= y2_min or y2_max <= y1_min:
-        return False
-    else:
-        return True
+def isCollisionRect(rect1, rect2):
+ 
+  if not isCorrectRect(rect1):
+    raise RectCorrectError("1й прямоугольник некорректный")
+  
+  if not isCorrectRect(rect2):
+    raise RectCorrectError("2й прямоугольник некорректный")
+  
+  x1, y1 = rect1[0]  
+  x2, y2 = rect1[1] 
+  
+  x3, y3 = rect2[0]  
+  x4, y4 = rect2[1]  
+  
+  return x1 < x4 and x2 > x3 and y1 < y4 and y2 > y3
